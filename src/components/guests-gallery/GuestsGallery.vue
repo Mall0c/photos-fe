eacc1afcf71ea61267b4cda7534a32ca09417b0a
Richard Steinbrecht <richardsteinbrecht@hotmail.de>
2025-01-29 19:41:58 +0100

    feat: close dialog now works on click outside of dialog. start design of dialog.

--- src/components/guests-gallery/GuestsGallery.vue
+++ src/components/guests-gallery/GuestsGallery.vue
@@ -1,48 +1,74 @@
 <script setup>
 import { ref, computed, onBeforeMount, useTemplateRef } from 'vue';
 
-const fileIds = ref(null)
+const imageIds = ref(null)
 const error = ref(null)
 const dialogRef = useTemplateRef("dialog")
 const dialogOpened = ref(false)
+const currentImageIdInDialog = ref(null)
 
-function toggleDialog(fileId) {
+/**
+ * Open the dialog for a picture, that shows the picture in bigger resolution,
+ * as well as more information like description, comments, uploader, etc.
+ */
+function openDialog(imageId) {
     if (dialogOpened.value === false) {
         dialogRef.value.showModal()
         dialogOpened.value = true
-    } else {
-        dialogRef.value.close()
-        dialogOpened.value = false
+        currentImageIdInDialog.value = imageId
     }
 }
 
-function closeModal(event) {
-    if (!event.target.contains(dialogRef.value)) return;
-    dialogRef.value.close()
+/**
+ * Close the dialog when the user clicks outside the dialog (on the backdrop)
+ */
+function closeDialog(event) {
+    const rect = dialogRef.value.getBoundingClientRect();
+    if (
+        rect.top <= event.clientY 
+        && event.clientY <= rect.top + rect.height 
+        && rect.left <= event.clientX 
+        && event.clientX <= rect.left + rect.width
+    ) {
+        // Do nothing
+    } else {
+        dialogRef.value.close()
+        dialogOpened.value = false
+        currentImageIdInDialog.value = null
+    }
 }
 
 /**
  * Fetch file names from backend.
  */
-function fetchFileIds() {
+function fetchImageIds() {
     fetch("http://localhost:3000/images-by-guests")
         .then(res => res.json())
-        .then(res => fileIds.value = res)
+        .then(res => imageIds.value = res)
         .catch(err => error.value = err)
 }
 
 onBeforeMount(() => {
-    fetchFileIds()
+    fetchImageIds()
 })
 </script>
 
 <template>
     <div class="image-container">
-        <div v-for="fileId in fileIds" class="image-preview-box">
-            <img :src="`http://localhost:3000/images/${fileId}`" @click="toggleDialog(fileId)" />
+        <div v-for="imageId in imageIds" class="image-preview-box">
+            <img :src="`http://localhost:3000/images/scaled/${imageId}`" @click="openDialog(imageId)" />
         </div>
     </div>
-    <dialog class="dialog-box" ref="dialog" @click="closeModal">Moin</dialog>
+    <dialog class="dialog-element" ref="dialog" @click="closeDialog">
+        <div class="dialog-container">
+            <div v-if="currentImageIdInDialog" class="dialog-container-picture">
+                <img :src="`http://localhost:3000/images/${currentImageIdInDialog}`">
+            </div>
+            <div class="dialog-description">
+                Bla
+            </div>
+        </div>
+    </dialog>
 </template>
 
 <style>
@@ -68,12 +94,34 @@ onBeforeMount(() => {
     height: 100%;
 }
 
-.dialog-box {
-    padding: 0;
+.dialog-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: none;
+    border: none;
+    height: 90vh;
+    min-width: 30vw;
+    background-color: rgba(148, 148, 148, 0.9);
 }
 
-.dialog-box::backdrop {
+.dialog-element {
+    padding: 0;
+    border: 0;
+}
+
+.dialog-container::backdrop {
     background-color: rgba(148, 148, 148, 0.25);
+}
+
+.dialog-container-picture {
+    
+}
+
+.dialog-container-picture img {
+    object-fit: cover;
+    max-width: 800px;
+    max-height: 400px;
 }
 
 </style>
